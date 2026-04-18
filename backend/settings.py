a6d3ae3d0b4aad538dbfd60d25eccf0fda0f9997
GstvAlmeida <g-vinicius100@hotmail.com>
2026-04-18 03:15:56 -0300

    security: refactor settings to use env

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,11 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e94r%fols_e$=p4!)4*kfvv99qx@i0#=n@j8(ghbrrn4tbu6sd'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+load_dotenv()
 
 ALLOWED_HOSTS = []
 
